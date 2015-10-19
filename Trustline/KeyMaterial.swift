45a4b11b9bb67a832a9066b0211aa23b5d957f19
Amoriello Hutti <amoriello.hutti@gmail.com>
2015-10-20 00:22:42 +0200

    Adding KeyMaterial initialization from base64

--- Trustline/KeyMaterial.swift
+++ Trustline/KeyMaterial.swift
@@ -10,7 +10,7 @@ import Foundation
 
 
 class KeyMaterial {
-  let version :UInt8 = 1;
+  var version :UInt8 = 1;
   var passKey :[UInt8]?
   var crKey   :[UInt8]?
   var reqKey  :[UInt8]?
@@ -21,6 +21,22 @@ class KeyMaterial {
   init(crKey: [UInt8], reqKey: [UInt8]) {
     self.crKey = crKey
     self.reqKey = reqKey
+  }
+  
+  
+  init?(fromBase64 data: String) {
+    let res = NSData(base64EncodedString: data, options: NSDataBase64DecodingOptions.IgnoreUnknownCharacters)
+
+    if let data = res?.arrayOfBytes() {
+      if data.count == 49 {
+        version = data[0]
+        passKey = Array(data[1...16])
+        crKey = Array(data[17...32])
+        reqKey = Array(data[33...48])
+        return
+      }
+    }
+    return nil
   }
   
   func base64Data() -> NSData {
